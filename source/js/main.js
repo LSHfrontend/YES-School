a02f7a6d7bc67e9a2c6dd7e2d010a11e465e02fb
Yaroslav <lshfrontend@gmail.com>
2018-11-18 19:58:09 +0300

    Сделал инкапсуляцию и добавил закрытие при нажатии ESC

--- source/js/main.js
+++ source/js/main.js
@@ -1,37 +1,43 @@
 "use strict";
 
-let isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
+(function () {
+  let isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
 
-if (isIE11) {
-  let script = document.createElement("script");
-  script.src = "js/imagePolyfill.js";
-  document.body.appendChild(script);
-};
-
-if (window.outerWidth >= 760) {
-  let list = document.getElementsByClassName("reviews__list")[0];
-  let range = document.getElementById("reviews-range");
-  let listener = isIE11 ? "change" : "input";
-  range.addEventListener(listener, slider, false);
-
-  function slider() {
-    this.setAttribute("aria-valuenow", this.value);
-
-    list.getElementsByClassName("reviews__item--active")[0].classList.remove("reviews__item--active");
-    list.getElementsByClassName("reviews__item")[this.value - 1].classList.add("reviews__item--active");
+  if (isIE11) {
+    (function (arr) {
+      for (let i = 0; i < arr.length; i++) {
+        let script = document.createElement("script");
+        script.src = arr[i];
+        document.body.appendChild(script);
+      };
+    })(["js/imagePolyfill.js", "js/svgxuse.js"]);
   };
-};
 
-document.body.addEventListener("click", modalWindow, false);
+  if (window.outerWidth >= 760) {
+    let list = document.getElementsByClassName("reviews__list")[0];
+    let range = document.getElementById("reviews-range");
+    let listener = isIE11 ? "change" : "input";
 
-function modalWindow(evt) {
-  if (evt.target.getAttribute("href") !== "appointment.html") return;
-  evt.preventDefault();
+    function slider () {
+      this.setAttribute("aria-valuenow", this.value);
+
+      list.getElementsByClassName("reviews__item--active")[0].classList.remove("reviews__item--active");
+      list.getElementsByClassName("reviews__item")[this.value - 1].classList.add("reviews__item--active");
+    };
+
+    range.addEventListener(listener, slider, false);
+  };
 
   let modal = document.getElementsByClassName("appointment")[0];
-  let shading = document.getElementsByClassName("shading")[0];
 
-  modalWindow.showModal = function() {
+  function modalWindow(evt) {
+    if (evt.target.getAttribute("href") !== "appointment.html") return;
+    evt.preventDefault();
+
+    window.showModal();
+  };
+
+  window.showModal = function () {
     modal.style.display = "block";
     modal.getElementsByClassName("appointment__tel")[0].focus();
 
@@ -39,16 +45,21 @@ function modalWindow(evt) {
       (function() {
         let div = document.createElement("div");
         div.classList.add("shading");
-        div.setAttribute("onclick", "modalWindow.closeModal()")
+        div.setAttribute("onclick", "window.closeModal()")
         document.body.appendChild(div);
       })();
     };
   };
 
-  modalWindow.closeModal = function() {
+  window.closeModal = function () {
     modal.style.display = "none";
     document.body.removeChild(document.getElementsByClassName("shading")[0]);
   };
 
-  shading ? modalWindow.closeModal() : modalWindow.showModal();
-}
+  document.body.addEventListener("click", modalWindow, false);
+  document.body.addEventListener("keydown", function (evt) {
+    let shading = document.getElementsByClassName("shading")[0];
+    if (shading && evt.keyCode === 27) window.closeModal();
+  }, false);
+})();
+
